9aae362abff3f4ef3308a09cc5223f670a66a8e7
Andres Lopez <andreslopez@foodology.com.co>
2021-10-11 18:26:45 -0500

    first commit

---
+++ src/common/constants.ts
@@ -0,0 +1,11 @@
+const dbConfig = {
+  dialect: 'postgres',
+  host: 'rosimar.com.co',
+  port: 5432,
+  username: 'postgres',
+  password: 'rosimar',
+  database: 'rosimar',
+  user: 'postgres',
+};
+
+module.exports.dbConfig = dbConfig;
