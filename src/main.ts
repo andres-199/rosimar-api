f6c1d6a5cb07f1f9ca911fc109e40f7daf901624
Andres Lopez <andreslopez@foodology.com.co>
2021-11-08 09:12:16 -0500

    ssl added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const logger = new Logger('Main');
+let httpsOptions: any;
+try {
+  httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/rosimar.com.co/privkey.pem'),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/rosimar.com.co/fullchain.pem',
+    ),
+  }
+
+  logger.log('Leyendo certificado SSL');
+} catch (error) {
+  logger.log('No se pudo encontrar el certificado SSL');
+}
 
 async function bootstrap() {
   const volleyball = require('volleyball');
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true,httpsOptions });
 
   app.use(async (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
